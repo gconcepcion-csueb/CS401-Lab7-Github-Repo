81f2dd6be17d17151969dc95acb96c475b9a2a44
Gabriel Concepcion <gconcepcion2@horizon.csueastbay.edu>
2018-10-12 12:10:20 -0700

    Updated Hello.java with add() method

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,12 @@
 
 // prints hello
 public class Hello {
+    private static int add(int a, int b) {
+        return a + b;
+    }
+    
     public static void main(String [] args) {
         System.out.println("Hello");
+        System.out.println(add(4, 5));
     }
 }
